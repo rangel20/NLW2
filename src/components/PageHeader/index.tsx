5aa993411a984c218616061346c161b75821f157
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-04 02:32:49 -0300

    Form do TeacherLsit montado.

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,20 +8,22 @@ interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = () => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar"/>
+        </Link>
+        <img src={logoImg} alt="Proffy"/>
+      </div>
 
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+      <div className="header-content">
+        <strong>{props.title}</strong>
+        
+        {props.children}
+      </div>
+    </header>
   );
 };
 
