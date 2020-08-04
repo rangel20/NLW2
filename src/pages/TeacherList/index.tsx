6c32267d3a5f8984ab8e44845a656160c6a36d41
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-04 02:16:58 -0300

    Header componentizzado e incorporado.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 const TeacherList = () => {
   return (
-    <div>
-      TeacherList
+    <div id="page-teacher-list" className="container">
+      <PageHeader />
     </div>
   );
 };
