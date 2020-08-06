deef64b9a7a05d61e82b48840c969d97d81434a7
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-06 02:08:02 -0300

    Busca por porfessores funcionando.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,27 +4,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string,
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+};
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/29238500?s=460&u=86c4788399b6c116edd25c97933f8fd88c21b3fc&v=4" alt="Rangel Paolo"/>
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Rangel Paolo</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        Apaixonado por explodir coisas em laboratórios e po mudar a vida das pessoas através do experiências.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>RS 80,00</strong>
+          <strong>RS {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="WhatsApp"/>
